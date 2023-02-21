b8b3563038729b245ab64e0c06555647ceab712c
sneha_mandal <snehacreative2016@gmail.com>
2023-02-21 19:44:35 +0530

    tried setting github pages

--- metaSiteFrontend/src/pages/Contact.jsx
+++ metaSiteFrontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { ContactForm,Texts } from '../sections/Contact'
+// import { ContactForm,Texts } from '../sections/Contact'
  
 
 const Contact = () => {
@@ -13,17 +13,16 @@ contactNumber:"",
 
 
   return (
-    <div className='text-white relative '>
-    <div className=''>
-    <Texts/>
-
-     
+    <div className='text-white relative bg-black'>
+    <div className='bg-black'>
+    {/* <Texts/> */}
+Contacts page is still in progress     
     <div className='gradient-03 z-0'/>
      
    
 
 
-    <ContactForm/>
+    {/* <ContactForm/> */}
 
    </div>
     </div>
